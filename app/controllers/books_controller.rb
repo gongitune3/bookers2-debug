02e482729531546f28b88fd055841f0ca22a9b29
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-06-03 19:59:22 +0900

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,18 +5,19 @@ class BooksController < ApplicationController
 	  @mybook = Book.find(params[:id])
 	  @book = Book.new
 	  @user = User.find(@mybook.user_id)
+	  @book_comment = BookComment.new
   end
 
   def index
-	  @books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+	  @books = Book.all 
 	  @book = Book.new
   end
 
   def create
-	  @book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+		@book = Book.new(book_params) 
 		@book.user_id = current_user.id
-  	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to book_path(@book.id), notice: "successfully created book!"#保存された場合の移動先を指定。
+  	if	@book.save 
+  		redirect_to book_path(@book.id), notice: "successfully created book!"
   	else
   		@books = Book.all
   		render 'index'
@@ -36,7 +37,7 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   	if @book.update(book_params)
   		redirect_to @book, notice: "successfully updated book!"
-  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+  	else 
   		render "edit"
   	end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books,only: [:show,:index,:edit,:update,:destroy,:create] do
+    resources :book_comments, only: [:create, :destroy]
+  end
   root 'home#top'
   get 'home/about'
 end
