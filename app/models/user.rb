7e7b8fcb0e99458a1c1f52eb9de941359a08624e
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-06-05 23:09:06 +0900

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
 
-  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction,  length: {maximum: 50 }
   
@@ -20,12 +20,10 @@ class User < ApplicationRecord
     follower.create(followed_id: user_id)
   end
   
-  # ユーザーのフォローを外す
   def unfollow(user_id)
     follower.find_by(followed_id: user_id).destroy
   end
   
-  # フォローしていればtrueを返す
   def following?(user)
     following_user.include?(user)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
+
   resources :books,only: [:show,:index,:edit,:update,:destroy,:create] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
   root 'home#top'
   get 'home/about'
-  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
+  # post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  # post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
 end
